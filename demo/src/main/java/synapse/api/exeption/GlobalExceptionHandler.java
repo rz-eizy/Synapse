9493204d5346f37c996465eff9b3fa6a65e3182f
Alessandro <duartealessandroxd@gmail.com>
2026-06-03 15:55:11 -0400

    feat: implementar GlobalExceptionHandler y control de tamaño máximo de archivos
    Se creó la clase GlobalExceptionHandler bajo la anotación @RestControllerAdvice para centralizar y estandarizar el manejo de excepciones en toda la API. En esta primera etapa, se implementó el método handleMaxUploadSizeExceeded para interceptar la excepción MaxUploadSizeExceededException. Con esto, cuando una petición de subida de imágenes exceda el límite de tamaño configurado en la aplicación, el servidor responderá de manera controlada con un código de estado HTTP 400 Bad Request y un cuerpo JSON descriptivo para el cliente.

---
+++ demo/src/main/java/synapse/api/exeption/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package synapse.api.exeption;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Map<String, String>> handleMaxUploadSizeExceeded(MaxUploadSizeExceededException exc){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("Error", "El archivo subido excede el maximo permitido"));
+    }
+}
